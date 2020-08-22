33a3fe4e3aadbd80b17a198453d5b869494778bb
Felita Donor <donorfelita@msn.com>
2020-08-22 19:01:33 +0200

    Bug meta resolved

--- Quotes/Quotes/items.py
+++ Quotes/Quotes/items.py
@@ -1,17 +1,10 @@
-'''
-if you designed a relational database before, you may ask: should I have two items QuoteItem and AuthorItem to better represent the data logically? The answer is yes you could but not recommended in this case because items are returned by Scrapy in an asynchronous way and you will have additional logic added to match the quote item with its corresponding item — it’s much easier to put the related quote and author in one item in this case
-'''
-
 import scrapy
 from scrapy import Item, Field
 from scrapy.loader.processors import MapCompose, TakeFirst
-# MapCompose apply one or more processing functions to a Field
-# TakeFirst return the first value of the list
 from datetime import datetime
 
 
 def remove_quotes(text):
-    # strip : remove left and right characters
     text = text.strip(u'\u201c'u'\u201d')
     return text
 
--- Quotes/Quotes/spiders/quotes.py
+++ Quotes/Quotes/spiders/quotes.py
@@ -4,67 +4,32 @@ from Quotes.items import QuoteItem
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
+    allowed_domains = ['toscrape.com']
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
         self.logger.info('Parse function called on {}'.format(response.url))
         quotes = response.css('div.quote')
         
-        # Item Loader enables pre/post processing : raw data obtained from the css selector may need to be further parsed (quotation marks, birthday string, 'in' born location)
         for quote in quotes:
             loader = ItemLoader(item=QuoteItem(), selector = quote)
             loader.add_css('quote_content', '.text::text')
             loader.add_css('tags', '.tag::text')
             quote_item = loader.load_item()
-            '''
-            # Before Item Loader
-            yield {
-                'text': quote.css('.text::text').get(),
-                'author': quote.css('.author::text').get(),                'tags': quote.css('.tag::text').getall(),
-            }
-            '''
-            # Scrape the author's bio
             author_url = quote.css('.author + a::attr(href)').get() # + ?
-            # Goes to the author page + pass the metadata from one page to another
-            yield response.follow(author_url, callback = self.parse_author, meta = {'quote_item' : quote_item})
+            # Bug : replace meta 
+            # use the ``cb_kwargs`` functionality of :class:`~scrapy.http.Request` to pass a partially populated item.
+            yield response.follow(author_url,  self.parse_author, meta = {'quote_item' : quote_item})
         
-        '''   
-        # Scrape the next page : option 1
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            # get the full url 
-            next_page = response.urljoin(next_page)
-            # sends new request to the next page and call the same parse function to get the quotes from the new page
-            yield scrapy.Request(next_page, callback = self.parse)
-        '''
-        # Scrape the next page : option 2
         for a in response.css('li.next a'):
-            # 'follow' support relative urls and automatically use the 'href' for 'a'
-            yield response.follow(a, callback = self.parse)
+            yield response.follow(a, self.parse)
             
     def parse_author(self, response):
-        #  RuntimeError: To use XPath or CSS selectors, ItemLoadermust be instantiated with a selector
-        # Solution : add quotes loop + selector
-        # à investiger 
-        # Pb : items scraped = 0
-        quotes = response.css('div.quote')
-        
-        for quote in quotes:
-            # quote_item = response.meta['quote_item']
-            loader = ItemLoader(item = quote_item, selector = quote, reponse = response)
-            loader.add_css('author_name', '.author-title::text')
-            loader.add_css('author_birthday', '.author-born-date::text')
-            loader.add_css('author_bornlocation', '.author-born-location::text')
-            loader.add_css('author_bio', '.author-description::text')
-            yield loader.load_item()
-        '''
-        Before ItemLoader
-        # Asynchronous : may not be processed in sync with the corresponding quote
-        # 'author_name' same as 'author'
-        yield {
-            'author_name' : response.css('.author-title::text').get(),
-            'author_birthday' : response.css('.author-born-date::text').get(),
-            'author_bornlocation' : response.css('.author-born-location::text').get(),
-            'author_bio' : response.css('.author-description::text').get(),
-        }
-        '''
+        quote_item = response.meta['quote_item']
+        loader = ItemLoader(item = quote_item, response = response)
+        loader.add_css('author_name', '.author-title::text')
+        loader.add_css('author_birthday', '.author-born-date::text')
+        loader.add_css('author_bornlocation', '.author-born-location::text')
+        loader.add_css('author_bio', '.author-description::text')
+        yield loader.load_item()
+    
